0f15bd8c6e7317e5139c51c49adeab8c8ef6ffd7
Gaurav <gaurav@sarvam.ai>
2026-03-26 09:04:41 +0530

    print statement to check changes detected

--- arya/new_feature_dag.py
+++ arya/new_feature_dag.py
@@ -1,6 +1,7 @@
 from airflow.decorators import dag, task
 from pendulum import datetime
 
+
 @dag(
     dag_id="arya_new_feature",
     start_date=datetime(2024, 1, 1),
@@ -9,12 +10,15 @@ from pendulum import datetime
     tags=["arya", "new"],
 )
 def arya_new_feature():
-
     @task
     def do_something():
         print("This DAG appeared without any restart or image rebuild!")
+        print(
+            "This Dag line appeared which is made to check changes are picked up or not!!"
+        )
         return "magic"
 
     do_something()
+
 
 arya_new_feature()
